36983d9d3ac6692619fbdc3d80c1f37dac49e711
Navid Ramezani <124895301+navidrz@users.noreply.github.com>
2024-12-09 08:51:10 -0800

    Create AppProvider.tsx

---
+++ src/core/AppProvider.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ThemeProvider } from './ThemeContext';
+
+const AppProvider: React.FC = ({ children }) => {
+  return (
+    <ThemeProvider>
+      {children}
+    </ThemeProvider>
+  );
+};
+
+export default AppProvider;
